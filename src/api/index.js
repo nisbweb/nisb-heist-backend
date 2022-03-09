8fcf704dff2e650d640058d7ff3d2ca737496216
Shreesh Kulkarni <sgkul2000@gmail.com>
2022-03-09 20:51:07 +0530

    changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,41 +1,46 @@
-const express = require("express")
-const router = express.Router()
-const answers = require("../../answers.json")
+const express = require("express");
+const router = express.Router();
+const answers = require("../../answers.json");
 
 router.get("/", async (req, res) => {
   return res.json({
     success: true,
     submission: false,
     message: "You have reached the API 🎉",
-  })
-})
+  });
+});
 
 function Normalize(answer) {
-  return answer.replace(/\s/g, "").toLowerCase()
+  return answer.replace(/\s/g, "").toLowerCase();
 }
 
 function Match(submission, answers) {
   for (let i = 0; i < answers.length; i++) {
     if (Normalize(answers[i]) === Normalize(submission)) {
-      return true
+      return true;
     }
   }
-  return false
+  return false;
 }
 
 router.post("/submit", async (req, res, next) => {
-  console.log(req.body)
+  console.log(req.body);
+  if (!request.body.question || !request.body.answer) {
+    return res.statue(400).json({
+      error: "Invalid answer",
+    });
+  }
   const question = answers.find((element) => {
-    return element.question === req.body.question
-  })
+    return element.question === req.body.question;
+  });
   if (!question) {
-    next(new Error("Invalid question number"))
+    next(new Error("Invalid question number"));
   }
 
   return res.status(200).json({
     success: true,
     submission: Match(req.body.answer, question.answers),
-  })
-})
+  });
+});
 
-module.exports = router
+module.exports = router;
